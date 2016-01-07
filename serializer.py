a60cba251f4794595f3dd932df5c64f0ac065cae
Julie Pan <ycp217@nyu.edu>
2016-01-06 16:47:52 -0800

    add Document class in case we ever use meta data

--- serializer.py
+++ serializer.py
@@ -2,6 +2,21 @@
 import json
 
 
+class Document(object):
+    def __init__(self):
+        self.s = {}
+
+    def set_data(self, data):
+        if data.__class__.__name__ == 'Resource':
+            self.s['data'] = data.s
+
+    def to_json(self, **kwargs):
+        if kwargs.get('indent') is not None:
+            return json.dumps(self.s, indent=kwargs['indent'])
+        else:
+            return json.dumps(self.s)
+
+
 class Link(object):
     def __init__(self, resource_type, resource_name):
         self.s = {'type': resource_type,
@@ -11,35 +26,31 @@ class Link(object):
 class Resource(object):
     def __init__(self, resource_type, attributes, relationships):
         self.s = {}
-        self.s['data'] = {}
-        self.s['data']['type'] = resource_type
-        self.s['data']['attributes'] = {}
-        self.s['data']['relationships'] = {}
+        self.s['type'] = resource_type
+        self.s['attributes'] = {}
+        self.s['relationships'] = {}
 
         for attr_name in attributes:
-            self.s['data']['attributes'][attr_name] = attributes[attr_name]
+            self.s['attributes'][attr_name] = attributes[attr_name]
 
         for relationship_name in relationships:
             if relationships[relationship_name].__class__.__name__ == 'Link':
                 # only one Link and should be added when object is created
                 rel = relationships[relationship_name]
-                self.s['data']['relationships'][relationship_name] = {}
-                self.s['data']['relationships'][relationship_name]['data'] = rel.s
+                self.s['relationships'][relationship_name] = {}
+                self.s['relationships'][relationship_name]['data'] = rel.s
 
     def add_relationship(self, relationship_name, resource):
-        if self.s['data']['relationships'].get(relationship_name) is None:
-            self.s['data']['relationships'][relationship_name] = {}
-            self.s['data']['relationships'][relationship_name]['data'] = []
+        if self.s['relationships'].get(relationship_name) is None:
+            self.s['relationships'][relationship_name] = {}
+            self.s['relationships'][relationship_name]['data'] = []
         if resource.__class__.__name__ == 'Link':
-            self.s['data']['relationships'][relationship_name]['data'].append(resource.s)
+            self.s['relationships'][relationship_name]['data'].append(resource.s)
 
-    def to_json(self, **kwargs):
-        if kwargs.get('indent') is not None:
-            return json.dumps(self.s, indent=kwargs['indent'])
-        else:
-            return json.dumps(self.s)
 
 # SAMPLE USAGE
+survey_doc = Document()
+
 question_ids = [
     '5647fe7c9bf910ffbbcae5db',
     '5647fe14f374f67c68805ff4',
@@ -52,19 +63,25 @@ question_ids = [
     '5647fe685f92f23c34bc893b'
     ]
 
+# define attributes
 survey_attributes = {
     'title': 'Tech@NYU Feedback: Venture Capital Panel',
-    'uri': 'https://techatnyu.typeform.com/to/iyscbb',
+    'URI': 'https://techatnyu.typeform.com/to/iyscbb',
     'responseVisibleTo': ['TEAM_MEMBER']
 }
 
+# define single item Link
 survey_relationships = {
     'addedBy': Link('people', '544195bba07c236a039e9016')
 }
 
-survey_doc = Resource('surveys', survey_attributes, survey_relationships)
+survey_resource = Resource('surveys', survey_attributes, survey_relationships)
+
+# how to add multi-Link relationship
 for qid in question_ids:
-    survey_doc.add_relationship('questions', Link('questions', qid))
+    survey_resource.add_relationship('questions', Link('questions', qid))
+
+survey_doc.set_data(survey_resource)
 
 # output json for api
 print survey_doc.to_json()
